a616713372ef5c2e84263f3d9b824a46ee33f626
vishalrami-coder <vishal@imavrix.com>
2026-04-01 18:33:22 +0530

    html change blog

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/nakshNewPage">
       <Provider store={store}>
-        <ScrollToTop />
+        {/* <ScrollToTop /> */}
         <App />
       </Provider>
     </BrowserRouter>
