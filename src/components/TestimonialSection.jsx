1ba29838816d5d8ead217392e1982cddf28313db
vishalrami-coder <vishal@imavrix.com>
2026-04-13 11:56:00 +0530

    changes done

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,97 +1,104 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Controller, Navigation } from "swiper/modules";
-import { useState } from "react";
+import { Navigation } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
+import "../assets/css/TestimonialSection.css";
+
+import AdaniWilmarLogo from "../assets/images/clients/adani-wilmar.webp";
+import AmulLogo from "../assets/images/clients/amul.webp";
+import CocaColaLogo from "../assets/images/clients/coca-cola.webp";
+import Intaslogo from "../assets/images/clients/intas.webp";
+import ltLogo from "../assets/images/clients/lt.webp";
+import Ongclogo from "../assets/images/clients/ongc.webp";
+import Zyduslogo from "../assets/images/clients/zydus.webp";
+import Indianoillogo from "../assets/images/clients/indian-oil.webp";
 
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck, faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+// client Logo 
+import ArtechFurnitureLogo from "../assets/images/testLogo/Artech_Furniture_Logo.png";
+import ColourinnLogo from "../assets/images/testLogo/Colourinn.png";
+import MaddyfryLogo from "../assets/images/testLogo/Maddyfry-Foods-Pvt-Ltd.svg";
+import ReeflexLogo from "../assets/images/testLogo/Reeflex-Logo-2.png";
+import NiralPatelLogo from "../assets/images/testLogo/NiralPatel.jpg";
 
 
 
-import '../assets/css/TestimonialSection.css'
-import SectionTitle from "./SectionTitle";
+
+
+
 function TestimonialSection() {
-const testimonials = [
-    {
-        logo: "/assets/images/Maddyfry-Foods-Pvt-Ltd.svg",
-        text: "Reliable source for Siemens HMI and servo drives. We’ve been sourcing from them for over a year. Zero issues so far!",
-        name: "Mr. Niral Patel",
-        company: "Maddyfry Foods Pvt Ltd",
-        image: "/assets/images/NiralPatel.jpg",
-    },
-    {
-        logo: "/assets/images/Colourinn.png",
-        text: "Very professional and helpful team. They helped me select the right Siemens VFD for my application.",
-        name: "Mr. Jay Patel",
-        company: "Colourinn industries pvt. Ltd",
-        initials: "JP",
-    },
-    {
-        logo: "/assets/images/Artech_Furniture_Logo.png",
-        text: "Great experience with Naksh Technology. Excellent product and service. I give a five-star rating!",
-        name: "Mr. Amit Gajera",
-        company: "Artech Furniture Private Limited",
-        initials: "AG",
-    },
-    {
-        logo: "/assets/images/Reeflex-Logo-2.png",
-        text: "Leading Siemens Automation Solutions provider and largest stockist of Siemens products in Gujarat region.",
-        name: "Mr. Nearal Patel",
-        company: "Reeflex Packaging Private Limited",
-        initials: "NP",
-    },
-];
+    const testimonials = [
+        {
+            logo: MaddyfryLogo,
+            text: "Reliable source for Siemens HMI and servo drives. We’ve been sourcing from them for over a year. Zero issues so far!",
+            name: "Mr. Niral Patel",
+            company: "Maddyfry Foods Pvt Ltd",
+            image: NiralPatelLogo,
+        },
+        {
+            logo: ColourinnLogo,
+            text: "Very professional and helpful team. They helped me select the right Siemens VFD for my application.",
+            name: "Mr. Jay Patel",
+            company: "Colourinn industries pvt. Ltd",
+            initials: "JP",
+        },
+        {
+            logo: ArtechFurnitureLogo,
+            text: "Great experience with Naksh Technology. Excellent product and service. I give a five-star rating!",
+            name: "Mr. Amit Gajera",
+            company: "Artech Furniture Private Limited",
+            initials: "AG",
+        },
+        {
+            logo: ReeflexLogo,
+            text: "Leading Siemens Automation Solutions provider and largest stockist of Siemens products in Gujarat region.",
+            name: "Mr. Nearal Patel",
+            company: "Reeflex Packaging Private Limited",
+            initials: "NP",
+        },
+    ];
 
+    return (
+        <div className="clientSec CustomPeding">
+            <div className="container">
+                <div className="row">
 
-const handleThumbClick = (index) => {
-    setActiveIndex(index);
-
-    if (mainSwiper) mainSwiper.slideTo(index);
-    if (thumbSwiper) thumbSwiper.slideTo(index);
-};
-
-return (
-    <div class="clientSec Custompending">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <div class="clientLogoWrapper">
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/adani-wilmar.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/amul.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/coca-cola.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/intas.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/lt.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/ongc.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/zydus.webp" alt="" />
-                        </div>
-                        <div class="clientsItem">
-                            <img src="https://nakshtechnology.in/assets/images/newLogo/indian-oil.webp" alt="" />
+                    {/* Left Side Logos */}
+                    <div className="col-sm-12 col-md-6">
+                        <div className="clientLogoWrapper">
+                            {[
+                                AdaniWilmarLogo,
+                                AmulLogo,
+                                CocaColaLogo,
+                                Intaslogo,
+                                ltLogo,
+                                Ongclogo,
+                                Zyduslogo,
+                                Indianoillogo,
+                            ].map((logo, i) => (
+                                <div className="clientsItem" key={i}>
+                                    <img
+                                        src={logo}
+                                        alt={logo}
+                                    />
+                                </div>
+                            ))}
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="TestimonialWrapper">
-                        <div class="swiper TestimonialSlider">
+
+                    {/* Right Side Testimonials */}
+                    <div className="col-sm-12 col-md-6">
+                        <div className="TestimonialWrapper">
                             <Swiper
                                 modules={[Navigation]}
                                 navigation
                                 loop={true}
+                                speed={800} // smooth transition (default 300)
+                                autoplay={{
+                                    delay: 3000,
+                                    disableOnInteraction: false,
+                                }}
                                 className="TestimonialSlider"
                             >
                                 {testimonials.map((item, index) => (
@@ -103,7 +110,7 @@ return (
                                                     <img
                                                         src={item.logo}
                                                         alt=""
-                                                        style={{ mixBlendMode: "difference" }}
+                                                        style={index === 0 ? { filter: "invert(1)" } : undefined}
                                                     />
                                                 </div>
 
@@ -131,14 +138,13 @@ return (
                                     </SwiperSlide>
                                 ))}
                             </Swiper>
-
                         </div>
                     </div>
+
                 </div>
-            </div >
-        </div >
-    </div >
-);
+            </div>
+        </div>
+    );
 }
 
 export default TestimonialSection;
